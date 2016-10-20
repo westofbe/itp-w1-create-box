d0fd00e9cbd8343d7285e50cadc485d2bb571c60
Santiago Basulto <santiago.basulto@gmail.com>
2016-10-20 13:47:25 -0300

    Import fix + test idea

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,29 @@
 import unittest
 
-from create_box import main
+from create_box import create_box
+
+first_box_expected = """
+****
+****
+****
+""".lstrip()
+
+second_box_expected = """
+@
+""".lstrip()
+
+third_box_expected = """
+xxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxx
+""".lstrip()
 
 
 class TestCreateBox(unittest.TestCase):
-
     def test_box(self):
-        self.assertEqual(create_box(3, 4, '*'), '****\n****\n****\n')
+        self.assertEqual(create_box(3, 4, '*'), first_box_expected)
 
     def test_small_box(self):
-        self.assertEqual(create_box(1, 1, '@'), '@\n')
+        self.assertEqual(create_box(1, 1, '@'), second_box_expected)
 
+    # Add your own test using third_box_expected
